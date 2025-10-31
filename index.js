e0bcbcbcad566bab253a77b6e2747f92c2e43884
pedrojose762 <notebookaamp@gmail.com>
2025-10-30 21:31:28 -0300

    atualizado 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {SerialPort} = require('serialport');
 const {ReadlineParser} = require('@serialport/parser-readline');
 const mqtt = require('mqtt');
 
-const port = new SerialPort({path: 'COM5', baudRate: 9600});
+const port = new SerialPort({path: 'COM3', baudRate: 9600});
 const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
 
 const client = mqtt.connect('mqtt://broker.hivemq.com:1883');
